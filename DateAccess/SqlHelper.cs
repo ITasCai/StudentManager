d592ae19a9efe5aafbd110db5dfaf919faa7e834
孤独的蔬菜 <shuai19970217@163.com>
2017-06-16 17:24:34 +0800

    ado.net

--- DateAccess/SqlHelper.cs
+++ DateAccess/SqlHelper.cs
@@ -4,86 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
-using System.Data.SqlClient;
 using System.Data;
-
-namespace DateAccess
+using System.Data.SqlClient;
+namespace DataAccess
 {
     /// <summary>
     /// 静态帮助类
     /// </summary>
-     public static class SqlHelper
+    public static class SqlHelper
     {
         /// <summary>
         /// 设置连接字符串
         /// </summary>
-        private static readonly string strConn= ConfigurationManager.ConnectionStrings["ConnStr"].ToString();
+        private static readonly string strConn = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
 
         /// <summary>
-        /// 声明了一个SqlTransaction对象
+        /// 声明一个SqlTransaction对象
         /// </summary>
         private static SqlTransaction tran;
 
         /// <summary>
-        /// 声明了一个SqlConnection对象
+        /// 声明一个SqlConnection对象
         /// </summary>
         private static SqlConnection conn;
 
         /// <summary>
         /// 返回连接对象
         /// </summary>
-        public static SqlConnection Conn {
+        public static SqlConnection Conn
+        {
             get
             {
                 if (conn == null)
                 {
-                    //创建一个连接对象
                     conn = new SqlConnection(strConn);
-
                 }
                 return conn;
             }
         }
 
-        #region 执行增删改
+        #region 执行增删改 ExecuteNonQuery
         /// <summary>
         /// 执行增删改
         /// </summary>
-        /// <param name="cmdType">命令类型（Text或者Proc）</param>
-        /// <param name="strSql">命令文本（SQL语句或者存储过程名称）</param>
+        /// <param name="cmdType">命令类型(Text或者Proc)</param>
+        /// <param name="strSql">命令文本(SQL语句或者存储过程名称)</param>
         /// <param name="param">传递的参数</param>
         /// <returns>返回受影响的行数</returns>
         public static int ExecuteNonQuery(CommandType cmdType, string strSql, params SqlParameter[] param)
         {
-
-            //创建连接对象，打开数据源
-            SqlConnection con = new SqlConnection(strConn);
+            SqlConnection con = new SqlConnection(strConn);//创建一个连接对象，用来指定连接的数据源
             try
             {
-
-                //创建命令对象，用来执行sql语句
-                SqlCommand cmd = new SqlCommand(strSql, con);
-                //设置命令类型
-                cmd.CommandType = cmdType;
-
-                //如果参数不为空，添加参数
+                SqlCommand cmd = new SqlCommand(strSql, con);//创建一个命令对象，用来执行SQL语句
+                cmd.CommandType = cmdType;//设置命令类型
                 if (param != null)
                 {
-                    //添加参数
-                    cmd.Parameters.AddRange(param);
+                    cmd.Parameters.AddRange(param);//添加参数
                 }
-                //打开连接(最晚打开，最早关闭)
-                con.Open();
-
+                con.Open();//最晚打开，最早关闭
                 int count = cmd.ExecuteNonQuery();
-
-
-                //返回受影响的行数
-                return count;
+                return count;//返回受影响的行数
             }
             catch (Exception ex)
             {
-
                 //con.Close();
                 //throw;
                 Console.WriteLine(ex.Message);
@@ -96,83 +80,66 @@ namespace DateAccess
         }
         #endregion
 
-
-        #region 返回单个值（第一行第一列）
+        #region 返回单个值 ExecuteScalar<T>
         /// <summary>
-        /// 返回单个值（第一行第一列）
+        /// 返回单个值(第一行第一列)
         /// </summary>
-        /// <param name="cmdType">命令类型（Text或者Proc）</param>
+        /// <param name="cmdType">命令类型</param>
         /// <param name="strSql">SQL语句</param>
         /// <param name="param">传递的参数</param>
         /// <returns>返回自定义类型结果</returns>
         public static T ExecuteScalar<T>(CommandType cmdType, string strSql, params SqlParameter[] param)
         {
-
-            //创建连接对象，打开数据源
             SqlConnection con = new SqlConnection(strConn);
             try
             {
                 SqlCommand cmd = new SqlCommand(strSql, con);
                 cmd.CommandType = cmdType;
-
                 if (param != null)
                 {
                     cmd.Parameters.AddRange(param);
                 }
-
                 con.Open();
-                //返回单个结果
-                object result = cmd.ExecuteScalar();
-
-                T t = default(T);
-                t = (T)Convert.ChangeType(result, typeof(object));
-                return t;
+                object result = cmd.ExecuteScalar();//返回单个结果
+                if (result != null)
+                {
+                    return (T)Convert.ChangeType(result, typeof(T));//typeof(T)
+                }
+                return default(T);
             }
             catch (Exception ex)
             {
-
-                return default(T);
                 Console.WriteLine(ex.Message);
+                return default(T);
             }
             finally
             {
                 con.Close();
             }
-
         }
-
         #endregion
 
-
-        #region 返回多条数据（多行多列）
-
+        #region 查询多条数据 ExecuteReader
         /// <summary>
-        /// 返回多条数据（多行多列）
+        /// 查询多条数据(多行多列)
         /// </summary>
-        /// <param name="cmdType">命令类型（Text或者Proc）</param>
-        /// <param name="strSql">sql语句</param>
+        /// <param name="cmdType">命令类型</param>
+        /// <param name="strSql">SQL语句</param>
         /// <param name="param">传递的参数</param>
-        /// <returns></returns>
-        public static SqlDataReader ExedcuteReader(CommandType cmdType, string strSql, params SqlParameter[] param)
+        /// <returns>返回SqlDataReader对象</returns>
+        public static SqlDataReader ExecuteReader(CommandType cmdType, string strSql, params SqlParameter[] param)
         {
-
-            //创建连接对象，打开数据源
             SqlConnection con = new SqlConnection(strConn);
-
             try
             {
-                //创建命令对象，用来执行sql语句
                 SqlCommand cmd = new SqlCommand(strSql, con);
-                //设置命令类型
                 cmd.CommandType = cmdType;
-
                 if (param != null)
                 {
                     cmd.Parameters.AddRange(param);
-
                 }
                 con.Open();
-                //CommandBehavior.CloseConnection:当读取完dr的数据时，执行dr.Close()后，则 dr所依赖对的所有对象，都会关闭
+                //CommandBehavior.CloseConnection 当读取完dr的数据时,执行dr.Close()后,则dr所依赖的所有对象，都会关闭。
                 SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
                 return dr;
             }
@@ -182,49 +149,37 @@ namespace DateAccess
                 Console.WriteLine(ex.Message);
                 return null;
             }
-
         }
         #endregion
 
+        #region 查询结果集 DataSet和DataTable
 
-        #region 返回查询结果集
         /// <summary>
-        /// 返回查询结果集
+        /// 查询结果集
         /// </summary>
-        /// <param name="cmdType">命令类型（Text或者Proc）</param>
+        /// <param name="cmdType">命令类型</param>
         /// <param name="strSql">SQL语句</param>
         /// <param name="param">传递的参数</param>
         /// <returns>返回DataSet</returns>
         public static DataSet ExecuteDataSet(CommandType cmdType, string strSql, params SqlParameter[] param)
         {
-
             SqlConnection con = new SqlConnection(strConn);
-
             try
             {
-                //创建命令对象，用来执行sql语句
                 SqlCommand cmd = new SqlCommand(strSql, con);
-                //设置命令类型
                 cmd.CommandType = cmdType;
-
                 if (param != null)
                 {
                     cmd.Parameters.AddRange(param);
-
                 }
                 con.Open();
-                //创建一个数据适配器对象
-                SqlDataAdapter da = new SqlDataAdapter();
-                //创建一个DataSet实例，用于存放数据
-                DataSet ds = new DataSet();
-                //填充数据集
-                da.Fill(ds);
-
+                SqlDataAdapter da = new SqlDataAdapter(cmd);//创建一个数据适配器对象
+                DataSet ds = new DataSet();//创建一个DataSet的实例，用于存放数据
+                da.Fill(ds);//填充数据集
                 return ds;
             }
             catch (Exception ex)
             {
-
                 Console.WriteLine(ex.Message);
                 return null;
             }
@@ -232,80 +187,73 @@ namespace DateAccess
             {
                 con.Close();
             }
-
         }
-        #endregion
-
-
-
 
         /// <summary>
         /// 查询DataTable
         /// </summary>
-        /// <param name="cmdType">命令类型（Text或者Proc）</param>
-        /// <param name="strSql">SQL语句</param>
-        /// <param name="param">传递的参数</param>
-        /// <returns>返回DataSet</returns>
-
-        public static DataTable ExecuteDataTable(CommandType cmdType, string strSql, params SqlParameter[] param) {
-
-            DataSet ds = ExecuteDataSet(cmdType,strSql,param);
+        /// <param name="cmdType"></param>
+        /// <param name="strSql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static DataTable ExecuteDataTable(CommandType cmdType, string strSql, params SqlParameter[] param)
+        {
+            DataSet ds = ExecuteDataSet(cmdType, strSql, param);//返回DataSet
             return ds.Tables[0];
-
         }
-
-
+        #endregion
 
         #region 执行事务
         /// <summary>
         /// 执行事务操作
         /// </summary>
-        /// <param name="cmdType">命令类型（Text或者Proc）</param>
+        /// <param name="cmdType">命令类型</param>
         /// <param name="strSql">SQL语句</param>
         /// <param name="param">传递的参数</param>
-        /// <returns>返回DataSet</returns>
         public static void ExecuteTransaction(CommandType cmdType, string strSql, params SqlParameter[] param)
         {
-
             SqlCommand cmd = new SqlCommand(strSql, conn);
             cmd.CommandType = cmdType;
             if (param != null)
             {
-                //添加参数
-                cmd.Parameters.AddRange(param);
+                cmd.Parameters.AddRange(param);//添加参数
             }
             if (tran != null)
             {
-                //设置事务
-                cmd.Transaction = tran;
-
+                cmd.Transaction = tran;//设置事务
             }
             cmd.ExecuteNonQuery();
         }
 
+
+        /// <summary>
+        /// 开启事务
+        /// </summary>
         public static void BeginTransaction()
         {
-            //打开连接
-            Conn.Open();
-            //开启事务
-            tran = conn.BeginTransaction();
+            Conn.Open();//打开连接
+            tran = conn.BeginTransaction();//开启事务
         }
 
+
+        /// <summary>
+        /// 提交事务
+        /// </summary>
         public static void CommitTransaction()
         {
-            //提交事务
-            tran.Commit();
-            //关闭连接
-            Conn.Close();
+            tran.Commit();//提交事务
+            Conn.Close();//关闭连接
         }
 
+        /// <summary>
+        /// 回滚事务
+        /// </summary>
         public static void RollBackTransaction()
         {
-
-            //回滚事务
-            tran.Rollback();
-            Conn.Close();
+            tran.Rollback();//回滚事务
+            Conn.Close();//关闭连接
         }
         #endregion
+
     }
 }
--- StudentMS/Program.cs
+++ StudentMS/Program.cs
@@ -1,38 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DateAccess;
 using System.Data;
 using System.Data.SqlClient;
+using DateAccess;
+using DataAccess;
 
-
-namespace StudentMS
+namespace Deam
 {
-    class Program
+
+    namespace StudentMS
     {
-        static void Main(string[] args)
+        class Program
         {
-            // DataSet da= SqlHelper.ExecuteDataSet(CommandType.Text,"SELECT*FROM Students",null);
-
-
-            string strSQL1= "INSERT INTO Department(DeptName) VALUES('中央')";
-            string strSQL2 = "INSERT INTO Department(DeptName) VALUES('中央')";
-
-
-            try
+            static void Main(string[] args)
             {
+                //DataSet ds = SqlHelper.ExecuteDataSet(CommandType.Text, "SELECT * FROM Students", null);
 
-                SqlHelper.BeginTransaction();
-                SqlHelper.ExecuteDataTable();
-            }
-            catch (Exception)
-            {
+                //for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                //{
+                //    DataRow dr = ds.Tables[0].Rows[i];
+                //    for (int j = 0; j< ds.Tables[0].Columns.Count; j++)
+                //    {
+                //        Console.Write(dr[j] + "\t");
+                //    }
+                //    Console.WriteLine();
+                //}
 
-                throw;
+                string strsql1 = "insert into department(deptname) values('众阳')";
+                string strsql2 = "insert into department(deptname) values('众阳')";
+
+                try
+                {
+                    SqlHelper.BeginTransaction();
+                    SqlHelper.ExecuteTransaction(CommandType.Text, strsql1);
+                    SqlHelper.ExecuteTransaction(CommandType.Text, strsql2);
+                    SqlHelper.CommitTransaction();
+
+                }
+                catch (Exception ex)
+                {
+                    SqlHelper.RollBackTransaction();
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+
+                Console.ReadKey();
             }
-            Console.ReadKey();
         }
     }
 }
